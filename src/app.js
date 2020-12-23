aa97d0c287e7fccba686fe1eeb8c4335419fb33c
Aniruddha Singh Jafa <jafa.aniruddha@gmail.com>
2020-12-23 16:39:12 +0530

    Added 3 pages (about, help, weather), defined partials

--- src/app.js
+++ src/app.js
@@ -4,16 +4,34 @@ const express = require('express')
 const app = express()
 
 
+// Define paths
 const path = require('path')
 const publicPath = path.join(__dirname, '../public')
+const partialsPath = path.join(__dirname, '../views/partials')
 
 app.use(express.static(publicPath))  // use to serve static assets e.g. CSS
 
+
+// view engine
+const hbs = require('hbs')
+hbs.registerPartials(partialsPath)
 app.set('view engine', 'hbs')
 
+
+// Routes
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('weather', { title: 'Home', weather: 'sunnny' })
 })
+
+
+app.get('/about', (req, res) => {
+  res.render('about', {title: 'About'})
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {title: 'Help'})
+})
+
 
 
 
