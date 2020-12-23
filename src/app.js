c9910894f62ce033b2f83990cb49dcfc4aaf3ead
Aniruddha Singh Jafa <jafa.aniruddha@gmail.com>
2020-12-23 13:32:03 +0530

    Respond with static html file in /public

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
+const path = require('path')
+const publicPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicPath))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile('index.html')
 })
 
 
-const portToUse = 3000;
+
+const portToUse = 3000
 app.listen(portToUse, () => {
-  console.log(`Listening on port ${portToUse}...`);
+  console.log(`Listening on port ${portToUse}...`)
 })
