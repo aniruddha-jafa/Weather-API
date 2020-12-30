dab13d9479990e8446093a806d49c73aaac932fc
Amirudha Singh Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2020-12-30 15:45:31 +0530

    Added two helper functions for - form validation, rendering

--- src/app.js
+++ src/app.js
@@ -1,36 +1,48 @@
 'use strict'
 
+
+// import external modules
 const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
+// initialise app
 const app = express()
 
+// import helper functions (self-defined)
+const { validateQuery } = require('./helperFunctions')
+const { renderHomePage } = require('./helperFunctions')
 
-// Define paths
-const path = require('path')
+// Define useful variables
+const urlEncodedParser = express.urlencoded({ extended: false })
+const portToUse = 3000
+
+// Define paths for static assets
 const publicPath = path.join(__dirname, '../public')
 const partialsPath = path.join(__dirname, '../views/partials')
-
 app.use(express.static(publicPath))  // use to serve static assets e.g. CSS
 
-
-// view engine
-const hbs = require('hbs')
+// set view engine
 hbs.registerPartials(partialsPath)
 app.set('view engine', 'hbs')
 
-
 // Routes
-app.get('/', (req, res) => {
-  res.render('home', { title: 'Weather app', weather: 'sunnny' })
-})
+app.get('/', renderHomePage)
 
+app.post('/',
+  urlEncodedParser,
+  validateQuery,
+  renderHomePage,
+  (err, req, res, next) => {
+    console.error(err)
+  }
+)
 
 app.get('*', (req, res) => {
-  res.render('404', {title: 'Error 404'})
+  res.render('404', { title: 'Error 404' })
 })
 
 
-
-const portToUse = 3000
 app.listen(portToUse, () => {
   console.log(`Listening on port ${portToUse}...`)
 })
---
+++ src/helperFunctions.js
@@ -0,0 +1,36 @@
+'use strict'
+
+// imports
+const { body, check, validationResult } = require('express-validator')
+
+// render home page
+exports.renderHomePage = function(req, res) {
+  res.render('home', {
+            title: 'Weather app',
+            weather: 'Sunny',
+            error: 'dummy variable'
+          })
+}
+
+
+// sanitise & validate city name
+exports.validateQuery = [
+  check('city').exists(),
+  body('city').trim().escape(),
+  check('city').isLength({ min: 1 }),
+  check('city').isAlpha(),
+
+  (req, res, next) => {
+     const errors = validationResult(req)
+
+     if (!errors.isEmpty()) {
+       console.log(`Error detected! Passing to error handler`)
+       const errorToDisplay = errors.array()[0]
+       next(new Error(errorToDisplay.msg))
+
+     } else {
+       console.log('No validation errors detected')
+       next()
+      }
+    }
+]
